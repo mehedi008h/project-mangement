f41b076a4bbaf17a441f6cf0a5620c58de28f12a
Mehedi Hasan <mehedi08h@gmail.com>
2023-01-24 21:28:57 +0600

    design & validation auth form

---
+++ client/src/components/auth/Signup.jsx
@@ -0,0 +1,180 @@
+import {
+    Box,
+    Button,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Stack,
+    Text,
+} from "@chakra-ui/react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import React, { useState } from "react";
+
+// Creating schema
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .required("Email is a required field!")
+        .email("Invalid email format!"),
+    name: Yup.string().required("Name is a required field!"),
+    password: Yup.string()
+        .required("Password is a required field!")
+        .min(8, "Password must be at least 8 characters!"),
+});
+
+const Signup = ({ signup }) => {
+    // state
+    const [show, setShow] = useState(false);
+
+    // show password
+    const handleClick = () => setShow(!show);
+    return (
+        <Formik
+            validationSchema={schema}
+            initialValues={{
+                email: "",
+                password: "",
+            }}
+            onSubmit={(values) => {
+                console.log(values);
+            }}
+        >
+            {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+            }) => (
+                <form noValidate onSubmit={handleSubmit}>
+                    <Stack width="300px" spacing={4}>
+                        <Box>
+                            <Input
+                                type="name"
+                                border="1px"
+                                color="blackAlpha.700"
+                                borderColor={
+                                    errors.name && touched.name
+                                        ? "red"
+                                        : "blackAlpha.500"
+                                }
+                                focusBorderColor="blackAlpha.500"
+                                placeholder="Enter your name.."
+                                _placeholder={{
+                                    color: "blackAlpha.500",
+                                }}
+                                _hover={{
+                                    border: "1px",
+                                    borderColor: "blackAlpha.500",
+                                }}
+                                _focus={{ border: "1px" }}
+                                name="name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.name}
+                            />
+                            {/* If validation is not passed show errors */}
+                            <Text color="red.400" fontSize={14} marginTop={2}>
+                                {errors.name && touched.name && errors.name}
+                            </Text>
+                        </Box>
+                        <Box>
+                            <Input
+                                type="email"
+                                border="1px"
+                                color="blackAlpha.700"
+                                borderColor={
+                                    errors.email && touched.email
+                                        ? "red"
+                                        : "blackAlpha.500"
+                                }
+                                focusBorderColor="blackAlpha.500"
+                                placeholder="Enter your email.."
+                                _placeholder={{
+                                    color: "blackAlpha.500",
+                                }}
+                                _hover={{
+                                    border: "1px",
+                                    borderColor: "blackAlpha.500",
+                                }}
+                                _focus={{ border: "1px" }}
+                                name="email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.email}
+                            />
+                            {/* If validation is not passed show errors */}
+                            <Text color="red.400" fontSize={14} marginTop={2}>
+                                {errors.email && touched.email && errors.email}
+                            </Text>
+                        </Box>
+
+                        <Box>
+                            <InputGroup size="md">
+                                <Input
+                                    pr="4.5rem"
+                                    type={show ? "text" : "password"}
+                                    color="blackAlpha.700"
+                                    border="1px"
+                                    borderColor={
+                                        errors.password && touched.password
+                                            ? "red"
+                                            : "blackAlpha.500"
+                                    }
+                                    focusBorderColor="blackAlpha.500"
+                                    placeholder="Enter your password.."
+                                    _placeholder={{
+                                        color: "blackAlpha.500",
+                                    }}
+                                    _hover={{
+                                        border: "1px",
+                                        borderColor: "blackAlpha.500",
+                                    }}
+                                    _focus={{
+                                        border: "1px",
+                                    }}
+                                    name="password"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.password}
+                                />
+                                <InputRightElement width="4.5rem">
+                                    <Button
+                                        h="1.75rem"
+                                        size="sm"
+                                        onClick={handleClick}
+                                        color="blackAlpha.600"
+                                    >
+                                        {show ? (
+                                            <AiOutlineEyeInvisible size={20} />
+                                        ) : (
+                                            <AiOutlineEye size={20} />
+                                        )}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                            {/* If validation is not passed show errors */}
+                            <Text color="red.400" fontSize={14} marginTop={2}>
+                                {errors.password &&
+                                    touched.password &&
+                                    errors.password}
+                            </Text>
+                        </Box>
+                        <Button
+                            type="submit"
+                            bg="blue.600"
+                            color="white"
+                            _hover={{ bg: "blue.500" }}
+                        >
+                            {signup ? "Signup" : "Login"}
+                        </Button>
+                    </Stack>
+                </form>
+            )}
+        </Formik>
+    );
+};
+
+export default Signup;
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,10 +1,93 @@
-import React from "react";
+import { Box, Button, Center, Flex, Stack, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import Login from "../components/auth/Login";
+import Signup from "../components/auth/Signup";
+import { DiJira } from "react-icons/di";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
 
 const Auth = () => {
+    // state
+    const [signup, setSignup] = useState(false);
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Flex
+            height="100vh"
+            width="100%"
+            justify="space-between"
+            direction={signup ? "row-reverse" : "row"}
+        >
+            <Box display={{ md: "flex", base: "none" }} width="100%">
+                1
+            </Box>
+            <Center bg="white" width="100%">
+                <Stack
+                    align="center"
+                    justify="center"
+                    direction="column"
+                    gap={3}
+                >
+                    <DiJira size={60} color="darkcyan" />
+                    <Text color="black" fontSize="3xl" fontWeight="bold">
+                        {signup ? "Sign up" : "Log in"}
+                    </Text>
+                    {/* social media login  */}
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        gap={3}
+                        width="100&"
+                    >
+                        <Button
+                            leftIcon={<FcGoogle size={20} />}
+                            bg="blackAlpha.200"
+                            width="300px"
+                            _hover={{ bg: "blackAlpha.100" }}
+                            color="black"
+                            px={4}
+                            py={2}
+                        >
+                            Google
+                        </Button>
+                        <Button
+                            leftIcon={<FaFacebook color="#4267B2" size={20} />}
+                            bg="blackAlpha.200"
+                            width="300px"
+                            _hover={{ bg: "blackAlpha.100" }}
+                            color="black"
+                            px={4}
+                            py={2}
+                        >
+                            Facebook
+                        </Button>
+                    </Box>
+                    <Text
+                        color="blackAlpha.500"
+                        fontSize={14}
+                        textTransform="uppercase"
+                    >
+                        or with email
+                    </Text>
+                    {/* login & signup form  */}
+                    {signup ? (
+                        <Signup signup={signup} />
+                    ) : (
+                        <Login signup={signup} />
+                    )}
+
+                    <Button
+                        color="blackAlpha.700"
+                        fontSize={13}
+                        textTransform="uppercase"
+                        _hover={{ color: "blue.400" }}
+                        onClick={() => setSignup(!signup)}
+                    >
+                        {signup
+                            ? "Already have an account? Login"
+                            : "Don't have any account? Signup"}
+                    </Button>
+                </Stack>
+            </Center>
+        </Flex>
     );
 };
 
